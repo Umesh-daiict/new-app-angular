b99d30b131003749398e5f1c6c0c0401f0a1e7d8
umesh <umeshsavaliya007@gmail.com>
2025-01-12 11:34:48 +0530

    add username input and reset functionality in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  username=""
+  resetUserName(){
+    this.username =""
+  }
 }
