5af8f3a38e2b7a4cdccd360bf381ad9061672ce4
umesh <umeshsavaliya007@gmail.com>
2025-01-13 21:41:40 +0530

    Add button to display details and style for list items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  showData = false
+  clicksAt : string[]= []
+  handleShowDetails(){
+    this.clicksAt.push(new Date().toTimeString())
+    this.showData = true
+  }
 }
